5929144a31c2e8514d230c3239587be044987602
Clive <tranvinhnhan.tech.clone@gmail.com>
2021-05-31 16:48:13 +0700

    init

---
+++ nopCommerce-api/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using nopCommerce_api.Domain.Entities;
+using nopCommerce_api.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nopCommerce_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = _context.Set<Product>().ToList();
+            return products;
+        }
+    }
+}
---
+++ nopCommerce-api/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nopCommerce_api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nopCommerce_api.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
